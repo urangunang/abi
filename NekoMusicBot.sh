980333b5bb85fd75f5ffa0de985863f40320fa8c
urangunang <145663184+urangunang@users.noreply.github.com>
2023-10-26 12:02:48 +0700

    Create NekoMusicBot.sh

---
+++ NekoMusicBot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Clone repo
+git clone "https://github.com/urangunang/NekoMusicBot.git"
+
+# Masuk ke direktori repo
+cd NekoMusicBot
+
+# Install python3.10
+sudo apt-get install python3.10
+python3.10 -m venv venv
+source venv/bin/activate
+
+# Install requirements.txt
+pip install -r requirements.txt
+
+# Ubah nama berkas
+mv sample.env .env
+
+# Jalankan bash neko
+bash neko
