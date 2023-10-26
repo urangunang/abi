58dad7531ba9a7d6006abc8af5d6a3de1f9e9195
urangunang <145663184+urangunang@users.noreply.github.com>
2023-10-26 13:08:20 +0700

    Update NekoMusicBot.sh

--- NekoMusicBot.sh
+++ NekoMusicBot.sh
@@ -6,11 +6,28 @@ git clone "https://github.com/urangunang/NekoMusicBot.git"
 # Masuk ke direktori repo
 cd NekoMusicBot
 
-# Install Python 3.7.12 dengan pyenv
-pyenv install 3.7.12
+# Unduh Python 3.7
+wget https://www.python.org/ftp/python/3.7.12/Python-3.7.12.tgz
 
-# Menggunakan Python 3.7.12 dalam lingkungan virtual
-pyenv local 3.7.12
+# Ekstrak arsip Python 3.7
+tar -xzvf Python-3.7.12.tgz
+
+# Masuk ke direktori Python
+cd Python-3.7.12
+
+# Konfigurasi, kompilasi, dan instalasi Python
+./configure --enable-optimizations
+make
+sudo make install
+
+# Kembali ke direktori repo
+cd ..
+
+# Buat lingkungan virtual Python 3.7
+python3.7 -m venv venv
+
+# Aktifkan lingkungan virtual Python 3.7
+source venv/bin/activate
 
 # Install requirements.txt
 pip install -r requirements.txt
@@ -18,5 +35,5 @@ pip install -r requirements.txt
 # Ubah nama berkas
 mv sample.env .env
 
-# Jalankan bash neko dalam sesi Screen
-screen -dmS NekoMusicBot-session bash neko
+# Jalankan main.py
+python3 main.py
